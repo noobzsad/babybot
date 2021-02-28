1f8687a5713cea4bc4c35e17e44e4a95bd847295
Ramlan ID <77728778+Ramlan666@users.noreply.github.com>
2021-03-01 05:48:25 +0700

    Update ngab

--- install.sh
+++ install.sh
@@ -7,14 +7,9 @@ pkg install ffmpeg
 pkg install nodejs
 pkg install npm
 pkg install tesseract
-npm i node-fetch
-npm i base64-img
-npm i -g cwebp
-npm i -g ytdl
-npm i node-tesseract-ocr
 npm i
-npm i got
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+npm audit fix
 
 echo "[*] SUKSES BRO SILAHKAN KETIK \"node index.js\" Powered by Ramlan ID"
